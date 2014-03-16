923d917c10ae89ce104c813cedef2134bd4483d4
tangyue1004@gmail.com <tangyue1004@gmail.com@5bb49ded-a01b-44c8-bc22-4444fbe8f49f>
2014-03-16 06:26:19 +0000

    use TabularInline instead of StackedInline
    git-svn-id: https://svn.sinaapp.com/aalloy/1@12 5bb49ded-a01b-44c8-bc22-4444fbe8f49f

--- clip/admin.py
+++ clip/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from clip.models import Window, Material, Product, ProductTrim, ProductName
 
-class ProductInline(admin.StackedInline):
+class ProductInline(admin.TabularInline):
     model = Product
     extra = 0
 
 class WindowAdmin(admin.ModelAdmin):
     inlines = [ProductInline]
 
-class ProductTrimInline(admin.StackedInline):
+class ProductTrimInline(admin.TabularInline):
     model = ProductTrim
     extra = 0
 
