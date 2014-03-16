305c8b332b4fc884a0ff11b955f830eafef25f5a
tangyue1004@gmail.com <tangyue1004@gmail.com@5bb49ded-a01b-44c8-bc22-4444fbe8f49f>
2014-03-16 06:25:44 +0000

    init model
    git-svn-id: https://svn.sinaapp.com/aalloy/1@5 5bb49ded-a01b-44c8-bc22-4444fbe8f49f

---
+++ clip/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Window(models.Model):
+#    x = models.DecimalField(default=0)
+#    y = models.DecimalField(default=0)
+#    m = models.DecimalField(default=0)
+    name = models.CharField(max_length=50)
+
+class Material(models.Model):
+    name = models.CharField(max_length=50)
+
+class product(models.Model):
+    window = models.ForeignKey(Window)
+    name = models.CharField(max_length=50)
+    num = models.IntegerField(default=0)
+    formula = models.CharField(max_length=100)
+
+class product_trim(models.Model):
+    window = models.ForeignKey(Window)
+    material = models.ForeignKey(Material)
+    trim_length = models.DecimalField(default=0, max_digits=10, decimal_places=3)
