c871b2825b9c4487e95a471e0e1ec956511098eb
tangyue1004@gmail.com <tangyue1004@gmail.com@5bb49ded-a01b-44c8-bc22-4444fbe8f49f>
2014-03-16 06:28:03 +0000

    deal with post
    git-svn-id: https://svn.sinaapp.com/aalloy/1@23 5bb49ded-a01b-44c8-bc22-4444fbe8f49f

--- clip/views.py
+++ clip/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from clip.models import Window
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from decimal import Decimal
+import parser
 
 def index(request):
     context = {
@@ -10,6 +12,33 @@ def index(request):
     return render(request, 'clip/index.html', context)
 
 def submit(request):
+    windows = Window.objects.all()
+    results = []
+    for window in windows:
+        id = window.pk
+        has_num = request.POST.get('id_%d' % id, False);
+        if not has_num:
+            continue
+        num = int(request.POST.get('num_%d' % id, 0))
+        if num == 0:
+            continue
+        x = Decimal(request.POST.get('x_%d' % id, 0))
+        y = Decimal(request.POST.get('y_%d' % id, 0))
+        m = Decimal(request.POST.get('m_%d' % id, 0))
+        z = Decimal(request.POST.get('z_%d' % id, 0))
+        products = window.product_set.all()
+        for product in products:
+            formula = product.product_name.formula
+            a = product.producttrim_set.get(material_id=1).trim_length
+            code = parser.expr(formula).compile()
+            length = eval(code)
+            result = {
+                    'name' : product.product_name,
+                    'length' : length,
+                    'num' : num * product.num
+                    }
+            results.append(result)
+
     return HttpResponseRedirect(reverse('clip:result'))
 
 def result(request):
