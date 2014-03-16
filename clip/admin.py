1e4f06a71e0a11f61185234d5a971b6b9b433046
tangyue1004@gmail.com <tangyue1004@gmail.com@5bb49ded-a01b-44c8-bc22-4444fbe8f49f>
2014-03-16 06:26:10 +0000

    add admin view
    git-svn-id: https://svn.sinaapp.com/aalloy/1@10 5bb49ded-a01b-44c8-bc22-4444fbe8f49f

--- clip/admin.py
+++ clip/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from clip.models import Window, Material, Product, ProductTrim, ProductName
 
-# Register your models here.
+class ProductInline(admin.StackedInline):
+    model = Product
+    extra = 0
+
+class WindowAdmin(admin.ModelAdmin):
+    inlines = [ProductInline]
+
+class ProductTrimInline(admin.StackedInline):
+    model = ProductTrim
+    extra = 0
+
+class MaterialAdmin(admin.ModelAdmin):
+    inlines = [ProductTrimInline]
+
+admin.site.register(Window, WindowAdmin)
+admin.site.register(Material, MaterialAdmin)
+admin.site.register(Product)
+admin.site.register(ProductName)
+admin.site.register(ProductTrim)
